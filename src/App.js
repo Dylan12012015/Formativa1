7ed714187391c119937e9f0f92edbad8993f2681
Dylan12012015 <cardenasdylan45@gmail.com>
2024-12-03 22:13:57 -0500

    Nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Usuario } from './componentes/Usuario';
+import foto from './assets/img/foto.webp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 id="techo">We are committed expert partners</h1>
+        <p id="textoTecho">We place huge value on strong relationships and have seen the benefit they bring to our business. Customer feedback is vital in helping us to get it right</p>
       </header>
+      <div class="row">
+            <div class="container">
+                <div class="row py-5" id="cuadrito1">
+                    <div class="col-3">
+                        <img src={foto} id='fotico'></img>
+                    </div>
+                    <div class="col-9 text-center" id="textito">
+                    <Usuario texto="vitae suscipit tellus mauris a diam maecenas sed enim ut. Mauris augue neque gravidad in fermentum. Praesent semper feugiat nibh sed pulvinar proin" nombre="Nat Reynolds"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container">
+                <div class="row py-5" id="cuadrito2">
+                    <div class="col-3">
+                        <img src={foto} id='fotico'></img>
+                    </div>
+                    <div class="col-9 text-center" id="textito">
+                    <Usuario texto="vitae suscipit tellus mauris a diam maecenas sed enim ut. Mauris augue neque gravidad in fermentum. Praesent semper feugiat nibh sed pulvinar proin" nombre="CELIA ALMEDA"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container">
+                <div class="row py-5" id="cuadrito3">
+                    <div class="col-3">
+                        <img src={foto} id='fotico'></img>
+                    </div>
+                    <div class="col-9 text-center" id="textito">
+                    <Usuario texto="vitae suscipit tellus mauris a diam maecenas sed enim ut. Mauris augue neque gravidad in fermentum. Praesent semper feugiat nibh sed pulvinar proin" nombre="BOB ROBERTS"/>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    
   );
 }
 
---
+++ src/componentes/Usuario.js
@@ -0,0 +1,8 @@
+
+export const Usuario = ({texto, nombre}) => {
+    return <div>
+        
+        <p>"{texto}"</p>
+        <h4>{nombre}</h4>
+    </div>
+}
